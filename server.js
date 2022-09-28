3ad835ca7bd4a1ad60196a51135c9cc073172510
Sheila Donnelly <sheila.donnelly@outlook.com>
2022-09-28 02:04:41 -0700

    Added Express and third-party modules

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-//
-const connect = require('connect');
+//previously used to connect to connect with "require"
+// const connect = require('connect');
+
+//import express using es6+
+import express from "express";
+import cookieParser from "cookie-parser";
+import logger from 'morgan';
+import session from "express-session";
+
+//ES Modules fix for _dirname
+import path, {dirname} from 'path';
+import {fileURLToPath} from 'url';
+const _dirname = dirname(fileURLToPath(import.meta.url));
 
 //instantiate app server
-const app = connect();
+const app = express();
 
 //customer middleware (function)
 function helloWorld(req, res, next){
