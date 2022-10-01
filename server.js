0118b8d4befcb43d0902f55286698b0ed50af266
Sheila Donnelly <sheila.donnelly@outlook.com>
2022-09-30 20:24:34 -0700

    Static Files Working

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(_dirname, '../public')));
+app.use(express.static(path.join(_dirname, '/public')));
 app.use(session({
     secret: 'MySecret',
     saveUninitialied: false,
