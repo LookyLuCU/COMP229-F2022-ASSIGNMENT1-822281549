f7fc27d53e80da20bb756c84fea68103e61af325
Sheila Donnelly <sheila.donnelly@outlook.com>
2022-09-30 19:15:54 -0700

    Added folder structure, headers, fotters, nav

--- app/routes/index.route.server.js
+++ app/routes/index.route.server.js
@@ -1,18 +1,23 @@
 //INDEX ROUTES
 
 import { Router } from 'express';
-import { helloWorld } from '../controllers/index.controller.server.js'
+import { displayHomePage, 
+    displayAboutPage, 
+    displayProjectsPage, 
+    displayServicesPage, 
+    displayContactPage } from '../controllers/index.controller.server.js'
 
 //INSTANTIATING ABOVE ROUTER
 const router = new Router();
 
 //ROUTERcan be reused for multiple paths (see below)
 //app.use('/', helloWorld); moved from server.js
-router.get('/', helloWorld);    //root or home page link
-
-router.get('/hello', helloWorld);   //can be used for all my route paths
-router.get('/these', helloWorld);   //synced to matching controller code
-router.get('/work', helloWorld);
+router.get('/', displayHomePage);          //root or home page link
+router.get('/home', displayHomePage);   //can be used for all my route paths
+router.get('/about', displayAboutPage);     //synced to matching controller code
+router.get('/projects', displayProjectsPage); 
+router.get('/services', displayServicesPage); 
+router.get('/contact', displayContactPage); 
 
 //needs to be exportable for use
 export default router;
